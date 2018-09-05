9010721b21ed0de38642d48fc49ca2b6df8e9ca1
jkazan <johannes.k@live.com>
2018-09-05 19:19:50 +0200

    first commit

---
+++ emacs/.emacs.d/elpa/ws-butler-0.5/ws-butler-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ws-butler" "0.5" "Unobtrusively remove trailing whitespace." 'nil :url "https://github.com/lewang/ws-butler")
---
+++ emacs/.emacs.d/elpa/yascroll-0.1/yascroll-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yascroll" "0.1" "Yet Another Scroll Bar Mode" 'nil :keywords '("convenience"))
